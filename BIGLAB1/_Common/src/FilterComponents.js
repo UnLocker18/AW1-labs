0457406f04494bb08d79564ba60f9b691fd4956a
Nepec <alle.cre@gmail.com>
2021-05-08 08:35:46 +0200

    modifiche logica data

--- BIGLAB1/_Common/src/FilterComponents.js
+++ BIGLAB1/_Common/src/FilterComponents.js
@@ -1,40 +1,34 @@
-import Nav from 'react-bootstrap/Nav';
-import { Link, NavLink} from 'react-router-dom';
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
 
-import {filters} from './data';
+import { filters } from "./data";
 
 function Filter(props) {
-    return (
-        <NavLink 
-            to = {props.url}
-            className="rounded-0 border-top text-dark hover-bg-light nav-link"
-            onClick = {() => props.chooseFilter(props.url)}
-            >
-            {props.text}
-        </NavLink>
-    );
+  return (
+    <NavLink
+      to={props.url}
+      className="rounded-0 border-top text-dark hover-bg-light nav-link"
+      onClick={() => props.chooseFilter(props.url)}
+    >
+      {props.text}
+    </NavLink>
+  );
 }
 
 function FilterList(props) {
-    const filterList = filters.map( filter => <Filter key={filter.id} chooseFilter={props.chooseFilter} {...filter} />)
-    return (
-        <Nav variant="pills" className="flex-column" defaultActiveKey="1" id="filter-list">
-            {filterList}
-        </Nav>
-    );
+  const filterList = filters.map((filter) => (
+    <Filter key={filter.id} chooseFilter={props.chooseFilter} {...filter} />
+  ));
+  return (
+    <Nav
+      variant="pills"
+      className="flex-column"
+      defaultActiveKey="1"
+      id="filter-list"
+    >
+      {filterList}
+    </Nav>
+  );
 }
-
-/* function Filter(props) {
-    return <ListGroup.Item className={props.active && "bg-info border-info"} active={props.active}>{props.text}</ListGroup.Item>;
-}
-
-function FilterList() {
-    const filterList = filters.map( filter => <Filter key={filter.id} {...filter}/>)
-    return (
-        <ListGroup className="rounded-0">
-            {filterList}
-        </ListGroup>
-    );
-} */
 
 export default FilterList;
--- BIGLAB1/_Common/src/Main.js
+++ BIGLAB1/_Common/src/Main.js
@@ -1,122 +1,168 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
-import {filters, tl} from './data';
-import {TaskAdder} from './AdderComponents';
-import TaskList from './TaskComponents';
+import { tl } from "./data";
+import { TaskAdder } from "./AdderComponents";
+import TaskList from "./TaskComponents";
 
-import {Col, Container} from 'react-bootstrap';
+import { Col, Container } from "react-bootstrap";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault(dayjs.tz.guess());
 
 function Main(props) {
-    const [tasks, setTasks] = useState([...tl]);
-    const [editMode, setEditMode] = useState(-1);    
+  const [tasks, setTasks] = useState([...tl]);
+  const [editMode, setEditMode] = useState(-1);
 
-    const [show, setShow] = useState(false);
-    const [description, setDescription] = useState({value: '', isValid: false});
-    const [isPrivate, setIsPrivate] = useState(false);
-    const [isImportant, setIsImportant] = useState(false);
-    const [date, setDate] = useState({value: '', isValid: false});
-    const [time, setTime] = useState('00:00');
-    const [validated, setValidated] = useState(false);
+  const [show, setShow] = useState(false);
+  const [description, setDescription] = useState({ value: "", isValid: false });
+  const [isPrivate, setIsPrivate] = useState(false);
+  const [isImportant, setIsImportant] = useState(false);
+  const [date, setDate] = useState({ value: "", isValid: true });
+  const [time, setTime] = useState("00:00");
+  const [validated, setValidated] = useState(false);
 
-    const deleteTask = (tskID) => {
-        setTasks(tsks => tsks.filter(t => t.id !== tskID));
-    }
+  const deleteTask = (tskID) => {
+    setTasks((tsks) => tsks.filter((t) => t.id !== tskID));
+  };
 
-    const editTask = (tskID) => {
-        if(tskID >= 0) {
-            const currentTask = tasks.find(task => task.id === tskID);
+  const editTask = (tskID) => {
+    if (tskID >= 0) {
+      const currentTask = tasks.find((task) => task.id === tskID);
 
-            //qui la checkDescValidity dovrebbe essere inutile
-            setDescription({ value: currentTask.description, isValid: checkDescValidity(currentTask.description) });
-            setIsPrivate(currentTask.isPrivate);
-            setIsImportant(currentTask.isUrgent);
-            setDate({ value: currentTask.date.format("YYYY-MM-DD"), isValid: checkDateValidity(currentTask.date) });
-            setTime(currentTask.date.format("HH:MM"));
-        }
-        else clearForm();
+      //qui la checkDescValidity dovrebbe essere inutile
+      setDescription({
+        value: currentTask.description,
+        isValid: checkDescValidity(currentTask.description),
+      });
+      setIsPrivate(currentTask.isPrivate);
+      setIsImportant(currentTask.isUrgent);
+      if (!currentTask.date) currentTask.date = dayjs();
+      setDate({
+        value: currentTask.date.format("YYYY-MM-DD"),
+        isValid: checkDateValidity(currentTask.date, editMode),
+      });
+      setTime(currentTask.date.format("HH:MM"));
+    } else clearForm();
 
-        setEditMode(tskID);
-    };
-    
-    const handleShow = () => {
-        setEditMode(-1);
-        setShow(true);
-    }
-    const handleClose = () => {
-        setShow(false);
-        setValidated(false);
-    };
+    setEditMode(tskID);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        let id;        
+  const handleShow = () => {
+    setEditMode(-1);
+    setShow(true);
+  };
+  const handleClose = () => {
+    setShow(false);
+    setValidated(false);
+  };
 
-        if (description.isValid && date.isValid) {
-            if(editMode >= 0) {
-                id = editMode;
-                deleteTask(id);
-            }
-            else id = tasks.sort((a, b) => a.id - b.id)[tasks.length - 1].id + 1;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let id;
 
-            const task = { id: id, description: description.value, isPrivate: isPrivate, 
-                isUrgent: isImportant, date: dayjs.tz(`${date.value}T${time}:00.000Z`)
-            };
+    if (description.isValid && date.isValid) {
+      if (editMode >= 0) {
+        id = editMode;
+        deleteTask(id);
+      } else id = tasks.sort((a, b) => a.id - b.id)[tasks.length - 1].id + 1;
 
-            setTasks(tsks => [...tsks, task].sort((a, b) => a.id - b.id));
-            handleClose();
-            clearForm();
+      const task = {
+        id: id,
+        description: description.value,
+        isPrivate: isPrivate,
+        isUrgent: isImportant,
+        date: date.value && dayjs.tz(`${date.value}T${time}:00.000Z`),
+      };
 
-            setEditMode(-1);
-        }
-        else setValidated(true);        
-    };
+      setTasks((tsks) => [...tsks, task].sort((a, b) => a.id - b.id));
+      handleClose();
+      clearForm();
 
-    const handleDescChange = (ev) => {
-        setDescription({ value: ev.target.value, isValid: checkDescValidity(ev.target.value) });
-    };
+      setEditMode(-1);
+    } else setValidated(true);
+  };
 
-    const handleDateChange = (ev) => {
-        setDate({ value: ev.target.value, isValid: checkDateValidity(ev.target.value) });
-    };
+  const handleDescChange = (ev) => {
+    setDescription({
+      value: ev.target.value,
+      isValid: checkDescValidity(ev.target.value),
+    });
+  };
 
-    const handleTimeChange = (ev, value) => {
-        setTime(value);
-    };
+  const handleDateChange = (ev) => {
+    setDate({
+      value: ev.target.value,
+      isValid: checkDateValidity(ev.target.value, editMode),
+    });
+  };
 
-    const checkDescValidity = (desc) => !!desc;
+  const handleTimeChange = (ev, value) => {
+    setTime(value);
+  };
 
-    const checkDateValidity = (date) => dayjs(date) >= dayjs().startOf('d');
+  const checkDescValidity = (desc) => !!desc;
 
-    const clearForm = () => {
-        setValidated(false);
-        setDescription({value: '', isValid: false});
-        setIsPrivate(false);
-        setIsImportant(false);
-        setDate({value: '', isValid: false});
-        setTime('00:00');
-    };
-    
-    const formProps = {
-        lg: props.lg, editMode, editTask, description, isPrivate, isImportant, date, time, validated,
-        setIsPrivate, setIsImportant, handleSubmit, handleDateChange, handleDescChange, handleTimeChange
-    };
+  const checkDateValidity = (date, editMode) => {
+    if (!date) return true;
+    else if (editMode >= 0) return true;
+    else return dayjs(date) >= dayjs().startOf("d");
+  };
 
-    return (
-        <Col as="main" lg={8} className="py-3"><h1>{/*filters[props.activeFilter - 1].text*/}</h1>
-            <TaskList tasks={tasks} deleteTask={deleteTask} activeFilter={props.activeFilter} {...formProps} />
-            <Container fluid className="fixed-bottom d-flex flex-row-reverse px-4 mb-4">
-                <TaskAdder clearForm={clearForm} show={show} handleShow={handleShow} handleClose={handleClose} {...formProps} />
-            </Container>
-        </Col>
-    );
+  const clearForm = () => {
+    setValidated(false);
+    setDescription({ value: "", isValid: false });
+    setIsPrivate(false);
+    setIsImportant(false);
+    setDate({ value: "", isValid: true });
+    setTime("00:00");
+  };
+
+  const formProps = {
+    lg: props.lg,
+    editMode,
+    editTask,
+    description,
+    isPrivate,
+    isImportant,
+    date,
+    time,
+    validated,
+    setIsPrivate,
+    setIsImportant,
+    handleSubmit,
+    handleDateChange,
+    handleDescChange,
+    handleTimeChange,
+  };
+
+  return (
+    <Col as="main" lg={8} className="py-3">
+      <h1>{/*filters[props.activeFilter - 1].text*/}</h1>
+      <TaskList
+        tasks={tasks}
+        deleteTask={deleteTask}
+        activeFilter={props.activeFilter}
+        {...formProps}
+      />
+      <Container
+        fluid
+        className="fixed-bottom d-flex flex-row-reverse px-4 mb-4"
+      >
+        <TaskAdder
+          clearForm={clearForm}
+          show={show}
+          handleShow={handleShow}
+          handleClose={handleClose}
+          {...formProps}
+        />
+      </Container>
+    </Col>
+  );
 }
 
 export default Main;
