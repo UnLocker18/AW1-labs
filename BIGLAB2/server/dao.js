c66b2c2521cff79d506e88b2bff702e3fd38a9bd
Nepec <alle.cre@gmail.com>
2021-05-15 18:55:39 +0200

    Unito tutte le api + unif msg successo/errore

---
+++ BIGLAB2/server/dao.js
@@ -0,0 +1,190 @@
+"use strict";
+
+const sqlite = require("sqlite3");
+const dayjs = require("dayjs");
+
+var isBetween = require("dayjs/plugin/isBetween");
+var isToday = require("dayjs/plugin/isToday");
+
+dayjs.extend(isToday);
+dayjs.extend(isBetween);
+
+const db = new sqlite.Database("tasks.db", (err) => {
+  if (err) throw err;
+});
+
+//get all tasks
+exports.listTasks = () => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM tasks";
+    db.all(sql, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        const tasks = rows.map((task) => ({
+          id: task.id,
+          description: task.description,
+          important: task.important,
+          private: task.private,
+          deadline: task.deadline,
+          completed: task.completed,
+          user: task.user,
+        }));
+        resolve(tasks);
+      }
+    });
+  });
+};
+
+//get all tasks with a filter
+exports.listFilteredTasks = (filterName) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM tasks";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (rows === undefined)
+          resolve({ error: "Tasks not found in database" });
+        else {
+          const tasks = rows.map((task) => ({
+            id: task.id,
+            description: task.description,
+            important: task.important,
+            private: task.private,
+            deadline: task.deadline,
+            completed: task.completed,
+            user: task.user,
+          }));
+          const filteredTasks = applyFilter(filterName, tasks);
+          if (filteredTasks === undefined)
+            resolve({ error: "Filter not valid" });
+          else resolve(filteredTasks);
+        }
+      }
+    });
+  });
+};
+
+const applyFilter = (selected, list) => {
+  switch (selected) {
+    case "all":
+      return list;
+
+    case "important":
+      return list.filter((task) => task.important);
+
+    case "today":
+      return list.filter((task) => {
+        if (dayjs(task.deadline).isToday()) return true;
+        else return false;
+      });
+
+    case "next_7_days":
+      return list.filter((task) => {
+        if (
+          dayjs(task.deadline).isBetween(
+            dayjs().startOf("d").add(1, "day").subtract(1, "minute"),
+            dayjs().startOf("d").add(8, "day")
+          )
+        )
+          return true;
+        else return false;
+      });
+
+    case "private":
+      return list.filter((task) => task.private);
+
+    case "completed":
+      return list.filter((task) => task.completed);
+
+    default:
+      return undefined;
+  }
+};
+
+//get a task with a specific id
+exports.listTaskById = (taskId) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM tasks WHERE id = ?";
+    db.get(sql, [taskId], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (row == undefined) {
+          resolve({ error: "Task not found in database" });
+        } else {
+          const task = {
+            id: row.id,
+            description: row.description,
+            important: row.important,
+            private: row.private,
+            deadline: row.deadline,
+            completed: row.completed,
+            user: row.user,
+          };
+          resolve(task);
+        }
+      }
+    });
+  });
+};
+
+//create a task
+exports.newTask = (task) => {
+  return new Promise((resolve, reject) => {
+    const query =
+      "INSERT INTO tasks (description, important, private, deadline, user) VALUES(?,?,?,?,?)";
+    db.run(
+      query,
+      [task.description, task.important, task.private, task.deadline, 1],
+      function (err) {
+        if (err) {
+          reject(err);
+          return;
+        } else {
+          resolve(this.lastID);
+        }
+      }
+    );
+  });
+};
+
+//update task
+exports.updateTask = (task) => {
+  return new Promise((resolve, reject) => {
+    const sqlQuery =
+      "UPDATE tasks SET description = ?1, deadline = ?2, important = ?3, private = ?4, completed = ?5 WHERE id = ?6";
+    db.run(
+      sqlQuery,
+      [
+        task.description,
+        task.deadline,
+        task.important,
+        task.private,
+        task.completed,
+        task.id,
+      ],
+      function (err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(this.changes);
+      }
+    );
+  });
+};
+
+// delete an existing exam
+exports.deleteTask = (taskID) => {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "DELETE FROM tasks WHERE id = ?";
+    db.run(sqlQuery, [taskID], function (err) {
+      if (err) {
+        reject(err);
+        return err;
+      } else resolve(this.changes);
+    });
+  });
+};
---
+++ BIGLAB2/server/server.js
@@ -0,0 +1,209 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+const dao = require("./dao");
+const { body, validationResult, check, param } = require("express-validator");
+
+const app = express();
+const port = 3001;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+/******** API ********/
+
+// GET /api/tasks
+app.get(
+  "/api/tasks",
+  [param("id").isInt({ min: 0, max: 100 })],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ err: errors.array() });
+    }
+
+    try {
+      const tasks = await dao.listTasks();
+      res
+        .status(200)
+        .json({ status: "success", details: "api GET /tasks", content: tasks });
+    } catch {
+      res
+        .status(500)
+        .json({ status: "failure", details: `Database Error ${err}` });
+    }
+  }
+);
+
+// GET /api/tasks/:id
+app.get(
+  "/api/tasks/:id(\\d+)",
+  [param("id").isInt({ min: 0, max: 100 })],
+  async (req, res) => {
+    const id = req.params.id;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ err: errors.array() });
+    }
+
+    try {
+      const task = await dao.listTaskById(id);
+      res.status(200).json({
+        status: "success",
+        details: "api GET /tasks/:id",
+        content: task,
+      });
+    } catch {
+      res
+        .status(500)
+        .json({ status: "failure", details: `Database Error ${err}` });
+    }
+  }
+);
+
+// GET /api/tasks:filter
+app.get(
+  "/api/tasks/:filter",
+  [param("id").isInt({ min: 0, max: 100 })],
+  async (req, res) => {
+    const filter = req.params.filter;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ err: errors.array() });
+    }
+
+    try {
+      const tasks = await dao.listFilteredTasks(filter);
+      res.status(200).json({
+        status: "success",
+        details: `api GET /tasks/${filter}`,
+        content: tasks,
+      });
+    } catch {
+      res
+        .status(500)
+        .json({ status: "failure", details: `Database Error ${err}` });
+    }
+  }
+);
+
+//POST /api/tasks
+app.post(
+  "/api/tasks",
+  [
+    body("important").isBoolean(),
+    body("private").isBoolean(),
+    body("description").isLength({ min: 3, max: 100 }),
+    body("deadline")
+      .optional({ nullable: false, checkFalsy: true })
+      .isISO8601(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ err: errors.array() });
+    }
+
+    const task = {
+      description: req.body.description,
+      important: req.body.important,
+      private: req.body.private,
+      deadline: req.body.deadline,
+    };
+
+    dao
+      .newTask(task)
+      .then((id) =>
+        res.status(200).json({
+          status: "success",
+          details: `api POST /tasks with id: ${id}`,
+        })
+      )
+      .catch((err) =>
+        res
+          .status(500)
+          .json({ status: "failure", details: `Database Error ${err}` })
+      );
+  }
+);
+
+// PUT /api/tasks/:id
+app.put(
+  "/api/tasks/:id(\\d+)",
+  [
+    check("id")
+      .isInt({ min: 0, max: 100 })
+      .custom((value, { req }) => {
+        if (value != req.params.id) return false;
+        else return true;
+      }),
+    check("description").isLength({ min: 3, max: 100 }),
+    check("deadline")
+      .optional({ nullable: false, checkFalsy: true })
+      .isISO8601(),
+    check("important").isBoolean(),
+    check("private").isBoolean(),
+    check("completed").isBoolean(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const task = req.body;
+    dao
+      .updateTask(task)
+      .then((updatedTasks) => {
+        if (updatedTasks != 0)
+          return res
+            .status(200)
+            .json({ status: "success", details: `Updated task` });
+        else
+          return res
+            .status(404)
+            .json({ status: "failure", details: "Task not found" });
+      })
+      .catch((err) =>
+        res
+          .status(503)
+          .json({ status: "failure", details: `Database Error ${err}` })
+      );
+  }
+);
+
+// DELETE /api/tasks/:id
+app.delete(
+  "/api/tasks/:id(\\d+)",
+  [param("id").isInt({ min: 0, max: 100 })],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ err: errors.array() });
+    }
+
+    dao
+      .deleteTask(req.params.id)
+      .then((nDeletedTasks) => {
+        if (nDeletedTasks != 0)
+          return res.status(200).json({
+            status: "success",
+            details: `Deleted ${nDeletedTasks} tasks`,
+          });
+        else
+          return res
+            .status(404)
+            .json({ status: "failure", details: "Task not found" });
+      })
+      .catch((err) =>
+        res
+          .status(500)
+          .json({ status: "failure", details: `Database Error ${err}` })
+      );
+  }
+);
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
